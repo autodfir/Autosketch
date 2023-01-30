3c1c9feb50142073960ed300400905eaabff1f39
mzc96 <michalcichorz@gmail.com>
2023-01-30 22:50:03 +0100

    Deploy updates

--- README.md
+++ README.md
@@ -5,14 +5,23 @@ Framework to automate forensic workload and upload it to Timesketch
 
 # 1. Prerequisities 
  - docker-compose
- - running Timesketch
-
+ - Timesketch 
 
 # 2. Deployment
- - copy etc directory to opt/autosketch/timesketch
+### Automated
+```
+cd /opt
+curl https://github.com/autodfir/Autosketch/blob/main/deploy/deploy_as.sh > deploy_as.sh
+chmod +x deploy_as.sh
+sudo ./deploy_as.sh
+```
+
+### Manual
+ - copy etc/config.yaml directory to /opt/autosketch/etc
  - copy docker-compose.yaml to /opt/autosketch
  - edit TS_IP and TS_PORT to IP and PORT of Timesketch
- - docker-compose up
+ - docker-compose up -d
+ 
 
 # 3. Access
 Access Autosketch by visiting http://127.0.0.1:5001
---
+++ deploy/deploy_as.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# This script is used to deploy Autosketch
+
+#check if docker-compose is installed
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Error: docker-compose is not installed.' >&2
+  exit 1
+fi
+#check if curl is installed
+if ! [ -x "$(command -v curl)" ]; then
+  echo 'Error: curl is not installed.' >&2
+  exit 1
+fi
+
+#create autosketch directory and etc
+mkdir Autosketch
+mkdir Autosketch/etc
+
+#copy necessary files
+curl https://github.com/autodfir/Autosketch/blob/main/etc/config.yaml > Autosketch/etc/config.yaml
+curl https://github.com/autodfir/Autosketch/blob/main/docker-compose.yaml > Autosketch/docker-compose.yaml
+
+#read Timesketch IP address and port
+echo "Please enter the IP address of the Timesketch server"
+read TIMESKETCH_IP
+echo "Please enter the port of the Timesketch server"
+read TIMESKETCH_PORT
+
+#ask if user wants to use continue if Timesketch is not running
+if ! curl -s -o /dev/null -I -w "%{http_code}" http://$TIMESKETCH_IP:$TIMESKETCH_PORT; then
+    echo "Timesketch is not running on $TIMESKETCH_IP:$TIMESKETCH_PORT"
+    echo "Do you want to continue? (y/n)"
+    read CONTINUE
+    if [ $CONTINUE != "y" ]; then
+        exit 1
+    fi
+fi
+
+#replace Timesketch IP and port in config.yaml
+sed -i "s/TS_IP_HERE/$TIMESKETCH_IP/g" Autosketch/etc/config.yaml
+sed -i "s/TS_PORT_HERE/$TIMESKETCH_PORT/g" Autosketch/etc/config.yaml
+
+#ask if user want to run docker-compose
+echo "Do you want to run docker-compose? (y/n)"
+read RUN
+if [ $RUN == "y" ]; then
+    cd Autosketch
+    docker-compose up -d
+fi
+
+#inform user that Autosketch is running on localost:5001
+echo "Autosketch is running visit"
+echo "http://localhost:5001"
+
+
+
+
